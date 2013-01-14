dfc81abb187df2bac96706a66735fd53ed174d7f
Nicolas Chambrier <naholyr@gmail.com>
2013-01-15 00:15:02 +0100

    Add some colors to 'keys' and twitter connection process

--- cli/keys.js
+++ cli/keys.js
@@ -1,10 +1,17 @@
 
-var twitter = require('../lib/twitter');
+var Twitter = require('twitter');
 var config = require('../lib/config');
+var color = require('../lib/color');
 
 module.exports = function () {
   config.load();
   var command = arguments[arguments.length - 1];
+  if (!command.key && !command.secret && !command.resetAccess) {
+    console.error();
+    console.error(color.error('  Error: option (--key, --secret, or --reset-access) required'));
+    command.help();
+    process.exit(0);
+  }
   if (command.key) {
     config.data.consumer_key = command.key;
   }
@@ -12,19 +19,22 @@ module.exports = function () {
     config.data.consumer_secret = command.secret;
   }
   if (!command.check) {
-    console.error('App\'s credentials validation has been skipped.');
+    console.error(color.warning('App\'s credentials validation has been skipped.'));
     save();
   } else {
-    twitter(function (err, t) {
+    (new Twitter({
+      consumer_key: config.data.consumer_key,
+      consumer_secret: config.data.consumer_secret
+    })).oauth.getOAuthRequestToken(function (err) {
       if (err) {
         console.error();
-        console.error('Error %s: %s', err.statusCode, err.data || String(err));
+        console.error(color.error('Error %s: %s'), err.statusCode, err.data || String(err));
         console.error('Check your consumer key and/or secret!');
-        console.error('You new configuration HAS NOT been saved.');
+        console.error('You new configuration ' + color.bold('HAS NOT') + ' been saved.');
         console.error();
         process.exit(1);
       }
-      console.log('App\'s credentials have been validated.');
+      console.log(color.success('App\'s credentials have been validated.'));
       save();
     });
   }
@@ -32,9 +42,10 @@ module.exports = function () {
     if (config.data.access_token_key || config.data.access_token_secret) {
       delete config.data.access_token_key;
       delete config.data.access_token_secret;
-      console.log('Your access tokens have been removed.');
+      console.log(color.warning('Your access tokens have been removed.'));
     }
     config.save();
-    console.log('Configuration has been saved in "%s"', config.file);
+    console.log(color.success('Configuration has been saved in "%s"'), config.file);
+    console.log('Run "' + command.parent._name + ' test" to try connection');
   }
 }
