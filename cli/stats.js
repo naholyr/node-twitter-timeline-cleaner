2b471f5b8526073c6ba538c4688ac4bd86e1732f
Nicolas Chambrier <naholyr@gmail.com>
2013-01-14 15:29:45 +0100

    Authentication

--- cli/stats.js
+++ cli/stats.js
@@ -1,10 +1,8 @@
 
 var twitter = require('../lib/twitter');
-var config = require('../lib/config');
 
 module.exports = function () {
-  twitter(keys, function (err, t) {
-    console.error(err);
-    throw new Error('TODO');
-  })
+  twitter(function (err, t) {
+    console.error('TODO');
+  });
 }
